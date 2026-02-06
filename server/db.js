c21b5a18208063660d752dc7c6299652121f5164
jaymark31 <bernaljaymarkmark@gmail.com>
2026-02-06 09:29:29 +0800

    Fix db.js for Neon and setup login route

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 let pool;
 
 if (process.env.NODE_ENV === "development") {
-  // Development Pool
+  // Local Development
   pool = new Pool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "postgres",
@@ -15,10 +15,10 @@ if (process.env.NODE_ENV === "development") {
     port: process.env.DB_PORT || 5432,
   });
 } else {
-  // Production Pool (Render Postgres)
+  // Production - Neon
   pool = new Pool({
-    connectionString: `postgres://${process.env.DB_USER || "postgres"}:${process.env.DB_PASSWORD || "033105"}@${process.env.DB_HOST}:${process.env.DB_PORT || 5432}/${process.env.DB_NAME}?options=project=${process.env.ENDPOINT_ID}`,
-    ssl: { rejectUnauthorized: false },
+    connectionString: process.env.DATABASE_URL, // full Neon URL
+    ssl: { rejectUnauthorized: false }, // required for Neon
   });
 }
 
