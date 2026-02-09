fe6dd4ea1809f55740fe08782a79b73d86cdcbec
jaymark31 <bernaljaymarkmark@gmail.com>
2026-02-09 15:23:58 +0800

    Fix PostgreSQL SSL connection for Neon DB

--- server/db.js
+++ server/db.js
@@ -6,9 +6,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: {
+    rejectUnauthorized: false, // required for Neon/Postgres cloud SSL
+  },
 });
 
 // Test connection
