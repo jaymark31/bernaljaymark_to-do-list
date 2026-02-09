e36dc3f159bcdf8f7f7142e7dee5de537eb1e1ab
jaymark31 <bernaljaymarkmark@gmail.com>
2026-02-09 16:49:01 +0800

    fix cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://bernaljaymark-to-do-list.vercel.app'
+    'https://bernaljaymark-to-do-list-150ek8qqa.vercel.app/'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
