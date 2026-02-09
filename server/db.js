5ef11790364e34caf149a8a5e8c16016119a718a
jaymark31 <bernaljaymarkmark@gmail.com>
2026-02-09 16:04:48 +0800

    fix: correctly parse database url

--- server/db.js
+++ server/db.js
@@ -6,7 +6,13 @@ dotenv.config();
 // Fix for "SECURITY WARNING" about sslmode
 let connectionString = process.env.DATABASE_URL;
 if (connectionString) {
-  connectionString = connectionString.replace(/(\?|&)sslmode=require/, '');
+  try {
+    const url = new URL(connectionString);
+    url.searchParams.delete('sslmode');
+    connectionString = url.toString();
+  } catch (err) {
+    console.error('Error parsing DATABASE_URL:', err);
+  }
 }
 
 const pool = new Pool({
