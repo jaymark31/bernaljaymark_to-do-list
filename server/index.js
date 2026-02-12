76fc873c6c51851f3c69509d83eef5145490f29e
jaymark31 <bernaljaymarkmark@gmail.com>
2026-02-12 10:07:18 +0800

    sinuktak jy link nga pabaliw baliw

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ import cors from "cors";
 const allowedOrigins = [
   'http://localhost:5173', // local dev
   'https://bernaljaymark-to-do-git-8e356d-bernaljaymarkmark-5902s-projects.vercel.app',
-  'https://bernaljaymark-to-do-list-39liou645.vercel.app'
+  'https://bernaljaymark-to-do-list.vercel.app',
+  'https://bernaljaymark-to-do-list.vercel.app/'
 ];
 
 app.use(cors({
